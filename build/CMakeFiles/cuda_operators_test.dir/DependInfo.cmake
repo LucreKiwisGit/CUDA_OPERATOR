148e8f4512686a9192673f189cd0ca54c2efb9f4
Lucre <lucrekiwis@gmail.com>
2024-08-07 21:05:24 +0800

    ImplicitGemm_1

---
+++ build/CMakeFiles/cuda_operators_test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lucre/Project/Cuda_operator/kernels/implGEMM.cu" "CMakeFiles/cuda_operators_test.dir/kernels/implGEMM.cu.o" "gcc" "CMakeFiles/cuda_operators_test.dir/kernels/implGEMM.cu.o.d"
+  "/home/lucre/Project/Cuda_operator/main.cu" "CMakeFiles/cuda_operators_test.dir/main.cu.o" "gcc" "CMakeFiles/cuda_operators_test.dir/main.cu.o.d"
+  "/home/lucre/Project/Cuda_operator/test/implicitGemm.cu" "CMakeFiles/cuda_operators_test.dir/test/implicitGemm.cu.o" "gcc" "CMakeFiles/cuda_operators_test.dir/test/implicitGemm.cu.o.d"
+  "/home/lucre/Project/Cuda_operator/utils/getopt.cpp" "CMakeFiles/cuda_operators_test.dir/utils/getopt.cpp.o" "gcc" "CMakeFiles/cuda_operators_test.dir/utils/getopt.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cuda_operators_test.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_operators_test.dir/kernels/implGEMM.cu.o"
+  "CMakeFiles/cuda_operators_test.dir/kernels/implGEMM.cu.o.d"
+  "CMakeFiles/cuda_operators_test.dir/main.cu.o"
+  "CMakeFiles/cuda_operators_test.dir/main.cu.o.d"
+  "CMakeFiles/cuda_operators_test.dir/test/implicitGemm.cu.o"
+  "CMakeFiles/cuda_operators_test.dir/test/implicitGemm.cu.o.d"
+  "CMakeFiles/cuda_operators_test.dir/utils/getopt.cpp.o"
+  "CMakeFiles/cuda_operators_test.dir/utils/getopt.cpp.o.d"
+  "cuda_operators_test"
+  "cuda_operators_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/cuda_operators_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
