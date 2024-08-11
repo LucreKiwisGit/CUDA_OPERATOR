27bb8983107381826f9770b9a67c5bdf8888f621
Lucre <lucrekiwis@gmail.com>
2024-08-11 22:17:44 +0800

    GEMM TEST

--- build/CMakeFiles/cuda_operators_test.dir/DependInfo.cmake
+++ build/CMakeFiles/cuda_operators_test.dir/DependInfo.cmake
@@ -8,9 +8,11 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lucre/Project/Cuda_operator/kernels/GEMM.cu" "CMakeFiles/cuda_operators_test.dir/kernels/GEMM.cu.o" "gcc" "CMakeFiles/cuda_operators_test.dir/kernels/GEMM.cu.o.d"
   "/home/lucre/Project/Cuda_operator/kernels/implGEMM.cu" "CMakeFiles/cuda_operators_test.dir/kernels/implGEMM.cu.o" "gcc" "CMakeFiles/cuda_operators_test.dir/kernels/implGEMM.cu.o.d"
   "/home/lucre/Project/Cuda_operator/main.cu" "CMakeFiles/cuda_operators_test.dir/main.cu.o" "gcc" "CMakeFiles/cuda_operators_test.dir/main.cu.o.d"
-  "/home/lucre/Project/Cuda_operator/test/implicitGemm.cu" "CMakeFiles/cuda_operators_test.dir/test/implicitGemm.cu.o" "gcc" "CMakeFiles/cuda_operators_test.dir/test/implicitGemm.cu.o.d"
+  "/home/lucre/Project/Cuda_operator/test/test_GEMM.cu" "CMakeFiles/cuda_operators_test.dir/test/test_GEMM.cu.o" "gcc" "CMakeFiles/cuda_operators_test.dir/test/test_GEMM.cu.o.d"
+  "/home/lucre/Project/Cuda_operator/test/test_implicitGemm.cu" "CMakeFiles/cuda_operators_test.dir/test/test_implicitGemm.cu.o" "gcc" "CMakeFiles/cuda_operators_test.dir/test/test_implicitGemm.cu.o.d"
   "/home/lucre/Project/Cuda_operator/kernels/directConv2d.cc" "CMakeFiles/cuda_operators_test.dir/kernels/directConv2d.cc.o" "gcc" "CMakeFiles/cuda_operators_test.dir/kernels/directConv2d.cc.o.d"
   "/home/lucre/Project/Cuda_operator/utils/getopt.cpp" "CMakeFiles/cuda_operators_test.dir/utils/getopt.cpp.o" "gcc" "CMakeFiles/cuda_operators_test.dir/utils/getopt.cpp.o.d"
   )
